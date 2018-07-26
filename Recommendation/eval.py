82a5826d0c9bb5c686992997f55e4bd201e578c9
wenwen ye <yewenwen3@jd.com>
2018-07-26 23:08:02 +0800

    add eval

---
+++ Recommendation/eval.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+
+from hyperparams import Hyperparams as hp
+from data_load import *
+from self_attention import *
+from preprocess import *
+from train import Graph
+
+def eval():
+    g = Graph(is_training=False)
+    print "Graph loaded"
+
+    # load test data
+    X_test, U_test, Y_test = load_test_data()
+    user2idx, idx2user, item2idx, idx2item = load_all_dict(hp.fname)
+
+    with g.graph.as_default():
+        sv = tf.train.Supervisor()
+        with sv.managed_session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+            ## Restore parameters
+            sv.saver.restore(sess, tf.train.latest_checkpoint(hp.logdir))
+            print("Restored!")
+
+            ## Get model name
+            mname = open(hp.logdir + '/checkpoint', 'r').read().split('"')[1]  # model name
+
+            ## Inference
+            if not os.path.exists('results'): os.mkdir('results')
